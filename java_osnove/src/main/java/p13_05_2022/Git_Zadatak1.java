273964ba86a91ff028dfc6e94116f65d7e35cb80
Jelena Obradovic <vuckovic.jelena@gmail.com>
2022-05-13 20:57:43 +0200

    apredavanja 13.05.Git Zadatak 1

---
+++ java_osnove/src/main/java/p13_05_2022/Git_Zadatak1.java
@@ -0,0 +1,8 @@
+package p13_05_2022;
+
+public class Git_Zadatak1 {
+    public static void main(String[] args) {
+        System.out.println("git clone, git status, git add naziv fajla, git commit -m opis," +
+                "git push, git restore");
+    }
+}
