1ed5eea0b990087fb1ca10c5dd947f30f58a296d
Jelena Obradovic <vuckovic.jelena@gmail.com>
2022-05-13 20:02:08 +0200

    Minor typo

--- java_osnove/src/main/java/p13_05_2022/zadatak_1.java
+++ java_osnove/src/main/java/p13_05_2022/zadatak_1.java
@@ -2,6 +2,6 @@ package p13_05_2022;
 
 public class zadatak_1 {
     public static void main(String[] args) {
-        System.out.println("Hello world");
-    }
+        System.out.println("Hello wo rld");
+        System.out.println("Hi");    }
 }
