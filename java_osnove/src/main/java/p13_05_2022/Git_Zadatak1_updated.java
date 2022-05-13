f8f4c7c7d5327b9191ef8a45042d4ccf16e5518e
Jelena Obradovic <vuckovic.jelena@gmail.com>
2022-05-13 21:50:22 +0200

    Delete class

--- java_osnove/src/main/java/p13_05_2022/Git_Zadatak1_updated.java
+++
@@ -1,8 +0,0 @@
-package p13_05_2022;
-
-public class Git_Zadatak1_updated {
-    public static void main(String[] args) {
-        System.out.println("git clone, git status, git add naziv fajla, git commit -m opis," +
-                "git push, git restore");
-    }
-}
