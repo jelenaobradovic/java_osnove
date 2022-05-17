495ea51b1ab69340c1be4cce7c7f64e4324f3347
Jelena Obradovic <vuckovic.jelena@gmail.com>
2022-05-17 14:14:41 +0200

    Domaci 16.05.2022. zadatak 4

---
+++ java_osnove/src/main/java/d_16_05_2022/Zadatak_4.java
@@ -0,0 +1,25 @@
+package d_16_05_2022;
+
+import java.util.Scanner;
+
+public class Zadatak_4 {
+    public static void main(String[] args) {
+        //Metoda prima dva parametra N i karakter za stampu. Metoda stampa N karaktera.
+        //Ako se za N prosledi 5 i za karakter prosledi npr kosa crta (/) stampa se=>/ / / / /
+
+        Scanner s = new Scanner(System.in);
+        System.out.println("Unesite broj: ");
+        int n = s.nextInt();
+        System.out.println("Unesite karakter:");
+        String karakter = s.next();
+
+        stampaKaraktera(n, karakter);
+    }
+
+    static void stampaKaraktera(int n, String karakter) {
+        for (int i = 0; i < n; i++) {
+            System.out.print(karakter + " ");
+
+        }
+    }
+}
