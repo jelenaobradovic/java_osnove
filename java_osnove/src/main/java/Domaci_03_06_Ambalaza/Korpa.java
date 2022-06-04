e93b5594c2c8da10ceac71f85f53c4c142392cc9
Jelena Obradovic <vuckovic.jelena@gmail.com>
2022-06-05 01:14:16 +0200

    Domaci 03.06.22. Zadatak Ambalaza

---
+++ java_osnove/src/main/java/Domaci_03_06_Ambalaza/Korpa.java
@@ -0,0 +1,55 @@
+package Domaci_03_06_Ambalaza;
+
+import java.util.ArrayList;
+
+public class Korpa {
+  //Kreirati klasu Korpa koja ima:
+    //niz ambalaza
+
+private ArrayList <Ambalaza> nizAmbalaza = new ArrayList<>();
+
+    //metodu dodaj ambalazu
+
+    public void dodajAmbalazu (Ambalaza a)
+    {this.nizAmbalaza.add(a);}
+
+
+    //metodu izbaci ambalazu, u funkciju se prosledjuje
+    // barkod flase (String) koju treba izbaciti
+
+
+    public void izbaciAmbalazu (String barkod)
+    {
+        for (int i = 0; i < this.nizAmbalaza.size(); i++) {
+            if (this.nizAmbalaza.get(i).getBarkod().equals(barkod))
+            {this.nizAmbalaza.remove(i);}
+
+        }
+    }
+    //privatnu metodu koja vraca cenu svih ambalaza sa
+    // popustom, metoda kao parametar prima popust.
+
+    private double cenaSvihAmbalaza (double popust)
+    {   double suma=0;
+        for (int i = 0; i <this.nizAmbalaza.size() ; i++) {
+           suma+=this.nizAmbalaza.get(i).cenaArtikla() ;
+        }
+        return suma-popust;
+    }
+
+    //metodu koja racuna i vraca ukupncu cenu korpe (sabira sve cene ambalze),
+    // kao parametar funkcije se prima Super karticu iz koje se cita popust.
+    //
+
+    public double cenaKorpe(SuperKartica superKartica)
+    {
+        double suma=0;
+            for (int i = 0; i <this.nizAmbalaza.size() ; i++) {
+                suma+=this.nizAmbalaza.get(i).cenaArtikla() ;
+            }
+            return suma-superKartica.getPopust();
+
+        }
+    }
+
+
