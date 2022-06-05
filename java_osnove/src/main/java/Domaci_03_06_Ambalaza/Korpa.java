bc18db936f14fd75cde3005ea44ea66dd7ce7f20
Jelena Obradovic <vuckovic.jelena@gmail.com>
2022-06-05 13:11:41 +0200

    Domaci 03.06.22. Izmena metode Zadatak Ambalaza

--- java_osnove/src/main/java/Domaci_03_06_Ambalaza/Korpa.java
+++ java_osnove/src/main/java/Domaci_03_06_Ambalaza/Korpa.java
@@ -3,53 +3,49 @@ package Domaci_03_06_Ambalaza;
 import java.util.ArrayList;
 
 public class Korpa {
-  //Kreirati klasu Korpa koja ima:
+    //Kreirati klasu Korpa koja ima:
     //niz ambalaza
 
-private ArrayList <Ambalaza> nizAmbalaza = new ArrayList<>();
+    private ArrayList<Ambalaza> nizAmbalaza = new ArrayList<>();
 
     //metodu dodaj ambalazu
 
-    public void dodajAmbalazu (Ambalaza a)
-    {this.nizAmbalaza.add(a);}
+    public void dodajAmbalazu(Ambalaza a) {
+        this.nizAmbalaza.add(a);
+    }
 
 
     //metodu izbaci ambalazu, u funkciju se prosledjuje
     // barkod flase (String) koju treba izbaciti
 
 
-    public void izbaciAmbalazu (String barkod)
-    {
+    public void izbaciAmbalazu(String barkod) {
         for (int i = 0; i < this.nizAmbalaza.size(); i++) {
-            if (this.nizAmbalaza.get(i).getBarkod().equals(barkod))
-            {this.nizAmbalaza.remove(i);}
+            if (this.nizAmbalaza.get(i).getBarkod().equals(barkod)) {
+                this.nizAmbalaza.remove(i);
+            }
 
         }
     }
     //privatnu metodu koja vraca cenu svih ambalaza sa
     // popustom, metoda kao parametar prima popust.
 
-    private double cenaSvihAmbalaza (double popust)
-    {   double suma=0;
-        for (int i = 0; i <this.nizAmbalaza.size() ; i++) {
-           suma+=this.nizAmbalaza.get(i).cenaArtikla() ;
+    private double cenaSvihAmbalaza(double popust) {
+        double suma = 0;
+        for (int i = 0; i < this.nizAmbalaza.size(); i++) {
+            suma += this.nizAmbalaza.get(i).cenaArtikla();
         }
-        return suma-popust;
+        return suma - popust;
     }
 
     //metodu koja racuna i vraca ukupncu cenu korpe (sabira sve cene ambalze),
     // kao parametar funkcije se prima Super karticu iz koje se cita popust.
     //
 
-    public double cenaKorpe(SuperKartica superKartica)
-    {
-        double suma=0;
-            for (int i = 0; i <this.nizAmbalaza.size() ; i++) {
-                suma+=this.nizAmbalaza.get(i).cenaArtikla() ;
-            }
-            return suma-superKartica.getPopust();
+    public double cenaKorpe(SuperKartica superKartica) {
+      return  cenaSvihAmbalaza(superKartica.getPopust());
 
-        }
+
     }
 
-
+}
